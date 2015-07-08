45abef9967b65b143aadef9e6eee49dfddc33958
Dmitrii Petukhov <dimart.sp@gmail.com>
2015-07-08 11:31:43 +0300

    [AppPortfolio]: add buttons and toasts
    I also disabled AppBar and deleted settings menu.

--- MyAppPortfolio/app/src/main/java/com/dimart/myappportfolio/MainActivity.java
+++ MyAppPortfolio/app/src/main/java/com/dimart/myappportfolio/MainActivity.java
@@ -2,37 +2,25 @@ package com.dimart.myappportfolio;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
+    private Toast mToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToast = Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void showMessage(View view) {
+//        mToast.cancel();
+        Button btn = (Button) view;
+        CharSequence msg = String.format(getString(R.string.btn_clicked), btn.getText());
+        mToast.setText(msg);
+        mToast.show();
     }
 }
