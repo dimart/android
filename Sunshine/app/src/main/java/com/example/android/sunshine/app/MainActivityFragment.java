5fdaa49713f674c3f4cf1961104b5f40b693556e
Dmitrii Petukhov <dimart.sp@gmail.com>
2015-07-08 15:14:30 +0300

    [Sunshine]: add simple list view

--- Sunshine/app/src/main/java/com/example/android/sunshine/app/MainActivityFragment.java
+++ Sunshine/app/src/main/java/com/example/android/sunshine/app/MainActivityFragment.java
@@ -5,12 +5,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+    private ArrayAdapter<String> mForecastAdapter;
 
     public MainActivityFragment() {
     }
@@ -18,6 +25,28 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        String[] forecastArray = {
+                "Today - Rainy - 50/40",
+                "Tomorrow - Sunny - 80/67"
+        };
+
+        List<String> weekForecast = new ArrayList<>(Arrays.asList(forecastArray));
+
+        mForecastAdapter = new ArrayAdapter<>(
+                // The current context (this fragment's parent activity)
+                getActivity(),
+                // ID of list item layout
+                R.layout.list_item_forecast,
+                // ID of the textView to populate
+                R.id.list_item_forecast_textview,
+                // Forecast data
+                weekForecast);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(mForecastAdapter);
+
+        return rootView;
     }
 }
