f1aeb215e840d228afcb493b94a8a7aa7151e667
Dmitrii Petukhov <dimart.sp@gmail.com>
2015-07-09 21:03:02 +0300

    [AppPortfolio]: update ASD to 22.2.0 & toast to snackbar

--- MyAppPortfolio/app/src/main/java/com/dimart/myappportfolio/MainActivity.java
+++ MyAppPortfolio/app/src/main/java/com/dimart/myappportfolio/MainActivity.java
@@ -1,31 +1,27 @@
 package com.dimart.myappportfolio;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
-    private Toolbar toolbar;
-    private Toast mToast;
+public class MainActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        mToast = Toast.makeText(this, "", Toast.LENGTH_SHORT);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
     }
 
     public void showMessage(View view) {
         Button btn = (Button) view;
         CharSequence msg = String.format(getString(R.string.btn_clicked), btn.getText());
-        mToast.setText(msg);
-        mToast.show();
+        Snackbar.make(view, msg, Snackbar.LENGTH_SHORT).show();
     }
 }
