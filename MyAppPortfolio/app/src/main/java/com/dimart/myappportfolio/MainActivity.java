2f85a5c93f3e86a960bb8370a0e0ddbb5f1d8bcb
Dmitrii Petukhov <dimart.sp@gmail.com>
2015-07-09 14:06:43 +0300

    [AppPortfolio]: add simple toolbar and update style

--- MyAppPortfolio/app/src/main/java/com/dimart/myappportfolio/MainActivity.java
+++ MyAppPortfolio/app/src/main/java/com/dimart/myappportfolio/MainActivity.java
@@ -1,23 +1,28 @@
 package com.dimart.myappportfolio;
 
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
+    private Toolbar toolbar;
     private Toast mToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mToast = Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mToast = Toast.makeText(this, "", Toast.LENGTH_SHORT);
     }
 
     public void showMessage(View view) {
-//        mToast.cancel();
         Button btn = (Button) view;
         CharSequence msg = String.format(getString(R.string.btn_clicked), btn.getText());
         mToast.setText(msg);
