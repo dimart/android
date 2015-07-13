88eb645a81c20c258c43a008c8a7a3d76b15725e
Dmitrii Petukhov <dimart.sp@gmail.com>
2015-07-13 14:43:02 +0300

    [Sunshine]: launch new detail activity on listItem click

--- Sunshine/app/src/main/java/com/example/android/sunshine/app/MainActivityFragment.java
+++ Sunshine/app/src/main/java/com/example/android/sunshine/app/MainActivityFragment.java
@@ -1,5 +1,6 @@
 package com.example.android.sunshine.app;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -12,6 +13,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -86,6 +88,15 @@ public class MainActivityFragment extends Fragment {
 
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent detailActivityIntent = new Intent(getActivity(), DetailActivity.class)
+                        .putExtra(Intent.EXTRA_TEXT, mForecastAdapter.getItem(i));
+                startActivity(detailActivityIntent);
+            }
+        });
 
         return rootView;
     }
