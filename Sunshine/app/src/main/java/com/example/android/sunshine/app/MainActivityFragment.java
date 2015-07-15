5b380a4663bcadcab1cc3d3bdee6c3ad5eeee7a5
Dmitrii Petukhov <dimart.sp@gmail.com>
2015-07-15 15:57:33 +0300

    [Sunshine]: refactor formatHighLows method

--- Sunshine/app/src/main/java/com/example/android/sunshine/app/MainActivityFragment.java
+++ Sunshine/app/src/main/java/com/example/android/sunshine/app/MainActivityFragment.java
@@ -118,12 +118,12 @@ public class MainActivityFragment extends Fragment {
 
         private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
 
-        private double celsiusToFahrenheit(double x) {
-            return (x * 1.8) + 32;
-        }
-
         private String getReadableDateString(long time) {
             return new SimpleDateFormat("EEE MMM dd").format(time);
+        }
+
+        private double celsiusToFahrenheit(double x) {
+            return (x * 1.8) + 32;
         }
 
         private String formatHighLows(double high, double low) {
@@ -133,12 +133,11 @@ public class MainActivityFragment extends Fragment {
                             getString(R.string.pref_units_metric));
 
             if (units != null && units.equals(getString(R.string.pref_units_imperial))) {
-                return Math.round(celsiusToFahrenheit(high))
-                        + "/"
-                        + Math.round(celsiusToFahrenheit(low));
-            } else {
-                return Math.round(high) + "/" + Math.round(low);
+                high = celsiusToFahrenheit(high);
+                low = celsiusToFahrenheit(low);
             }
+
+            return Math.round(high) + "/" + Math.round(low);
         }
 
         private String[] getWeatherDataFromJson(String forecastJsonStr, int numDays)
