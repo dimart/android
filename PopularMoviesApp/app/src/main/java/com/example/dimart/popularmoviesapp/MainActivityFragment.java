75174217341c75690230235aea346e95cf386e47
Dmitrii Petukhov <dimart.sp@gmail.com>
2015-07-20 17:46:14 +0300

    [PopularMoviesApp]: have fun with retrofit

--- PopularMoviesApp/app/src/main/java/com/example/dimart/popularmoviesapp/MainActivityFragment.java
+++ PopularMoviesApp/app/src/main/java/com/example/dimart/popularmoviesapp/MainActivityFragment.java
@@ -10,21 +10,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
+import retrofit.RestAdapter;
+import retrofit.http.GET;
+import retrofit.http.Path;
+import retrofit.http.Query;
 
 
 /**
@@ -82,6 +79,23 @@ public class MainActivityFragment extends Fragment {
         updateMovies();
     }
 
+    public interface TMDBService {
+        @GET("/3/movie/{mode}")
+        TMDBResponse movies(@Path("mode") String mode, @Query("api_key") String apiKey);
+    }
+
+    private class TMDBResponse {
+        String page;
+        List<Movie> results =  new ArrayList<>();
+
+        class Movie {
+            String title;
+            String poster_path;
+            String overview;
+            String release_date;
+        }
+    }
+
     private void updateMovies() {
         new FetchMoviesTask().execute("popular");
     }
@@ -89,102 +103,23 @@ public class MainActivityFragment extends Fragment {
     private class FetchMoviesTask extends AsyncTask<String, Void, Movie[]> {
         private final String LOG_TAG = FetchMoviesTask.class.getSimpleName();
 
-        private Movie[] getMovieDataFromJson(String moviesJsonStr)
-                throws JSONException {
-            final String TMDB_LIST = "results";
-            final String TMDB_TITLE = "title";
-            final String TMDB_OVERVIEW = "overview";
-            final String TMDB_DATE = "release_date";
-            final String TMDB_POSTER = "poster_path";
-            final String TMDB_VOTE = "vote_average";
-
-            JSONObject moviesJson = new JSONObject(moviesJsonStr);
-            JSONArray moviesArray = moviesJson.getJSONArray(TMDB_LIST);
-
-            Movie[] movies = new Movie[moviesArray.length()];
-
-            for (int i = 0; i < moviesArray.length(); i++) {
-                JSONObject movieData = moviesArray.getJSONObject(i);
-
-                movies[i] = new Movie.Builder(movieData.getString(TMDB_TITLE),
-                        movieData.getString(TMDB_OVERVIEW))
-                        .releaseDate(toDate(movieData.getString(TMDB_DATE)))
-                        .posterUrl(getPosterUrlFor(movieData.getString(TMDB_POSTER)))
-                        .rating(((float) movieData.getDouble(TMDB_VOTE)))
-                        .build();
-            }
-
-            return movies;
-        }
-
         @Override
         protected Movie[] doInBackground(String... params) {
-            HttpURLConnection urlConnection = null;
-            BufferedReader reader = null;
 
-            String moviesJsonStr = null;
+            RestAdapter restAdapter = new RestAdapter.Builder()
+                    .setEndpoint("http://api.themoviedb.org")
+                    .build();
+            TMDBService service = restAdapter.create(TMDBService.class);
 
-            try {
-                final String TMDB_BASE_URL =
-                        "http://api.themoviedb.org/3/movie/";
-                final String QUERY_TYPE = params[0]; // popular or top_rated
-                Uri builtUri = Uri.parse(TMDB_BASE_URL).buildUpon()
-                        .appendPath(QUERY_TYPE)
-                        .appendQueryParameter("api_key", BuildConfig.THE_MOVIE_DB_KEY)
-                        .build();
-
-                URL url = new URL(builtUri.toString());
-
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.connect();
-
-
-                InputStream inputStream = urlConnection.getInputStream();
-                StringBuffer buffer = new StringBuffer();
-                if (inputStream == null) {
-                    // Nothing to do.
-                    return null;
-                }
-                reader = new BufferedReader(new InputStreamReader(inputStream));
-
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    // Since it's JSON, adding a newline won't affect parsing
-                    // But it does make debugging a *lot* easier.
-                    buffer.append(line + "\n");
-                }
-
-                if (buffer.length() == 0) {
-                    return null; // No point in parsing.
-                }
-
-                moviesJsonStr = buffer.toString();
-
-            } catch (IOException e) {
-                Log.e(LOG_TAG, "Error ", e);
-                return null;
-            } finally {
-                if (urlConnection != null) {
-                    urlConnection.disconnect();
-                }
-                if (reader != null) {
-                    try {
-                        reader.close();
-                    } catch (final IOException e) {
-                        Log.e(LOG_TAG, "Error closing stream", e);
-                    }
-                }
+            List<TMDBResponse.Movie> response = service.movies(params[0], BuildConfig.THE_MOVIE_DB_KEY).results;
+            List<Movie> movies = new ArrayList<>();
+            for (TMDBResponse.Movie x : response) {
+                movies.add(new Movie.Builder(x.title, x.overview)
+                        .posterUrl(getPosterUrlFor(x.poster_path))
+                        .releaseDate(toDate(x.release_date))
+                        .build());
             }
-
-            try {
-                return getMovieDataFromJson(moviesJsonStr);
-            } catch (JSONException e) {
-                Log.e(LOG_TAG, e.getMessage(), e);
-                e.printStackTrace();
-            }
-
-            return null;
+            return movies.toArray(new Movie[movies.size()]);
         }
 
         @Override
