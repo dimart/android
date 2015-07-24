81032b1b330cc20d215b85b5c23e3219aa4a0b2d
Dmitrii Petukhov <dimart.sp@gmail.com>
2015-07-24 14:37:42 +0300

    [PopularMoviesApp]: return toolbar to MainActivity & polish UI

--- PopularMoviesApp/app/src/main/java/com/example/dimart/popularmoviesapp/view/activity/MainActivity.java
+++ PopularMoviesApp/app/src/main/java/com/example/dimart/popularmoviesapp/view/activity/MainActivity.java
@@ -2,19 +2,23 @@ package com.example.dimart.popularmoviesapp.view.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.dimart.popularmoviesapp.R;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
 
