64e1bcc4e1cb9abff9d283c963c47654e5df2393
Dmitrii Petukhov <dimart.sp@gmail.com>
2015-08-19 23:48:19 +0500

    [YMoney]: add toolbar & empty fragment

---
+++ YMoney/app/src/main/java/com/example/dimart/ymoney/MainFragment.java
@@ -0,0 +1,20 @@
+package com.example.dimart.ymoney;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Dmitrii Petukhov on 8/19/15.
+ */
+public class MainFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+}
